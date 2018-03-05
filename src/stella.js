6b7d4e1967ecf964f3c612056764dc5e19a1a78e
stellaverse <ajb@stellaverse.com>
2018-03-04 21:32:46 -0500

    DEBUG: Empty object issue.

--- src/stella.js
+++ src/stella.js
@@ -1,28 +1,23 @@
 
 
 
-var stella = {};
-
-stella.config = require('./config/config.js');
-
-stella.api = {
-	request : require('./api/request.js')
-};
-
-stella.up = {
-	upload : require('./up/upload.js'),
-	initializeUpload : require('./up/initializeUpload.js'),
-	uploadFile : require('./up/uploadFile.js'),
-	abortUpload : require('./up/abortUpload.js'),
-	finalizeUpload : require('./up/finalizeUpload.js'),
-	uploaders : require('./up/uploaders.js'),
-	uploads : require('./up/uploads.js')
-};
-
-stella.util = {
-	getActiveAccount : require('./util/getActiveAccount.js'),
-	setActiveAccount : require('./util/setActiveAccount.js'),
-	serialize : require('./util/serialize.js')
-};
-
-module.exports = stella;
+module.exports = {
+	config : require('./config/config.js'),
+	api : {
+		request : require('./api/request.js')
+	},
+	up : {
+		upload : require('./up/upload.js'),
+		initializeUpload : require('./up/initializeUpload.js'),
+		uploadFile : require('./up/uploadFile.js'),
+		abortUpload : require('./up/abortUpload.js'),
+		finalizeUpload : require('./up/finalizeUpload.js'),
+		uploaders : require('./up/uploaders.js'),
+		uploads : require('./up/uploads.js')
+	},
+	util : {
+		getActiveAccount : require('./util/getActiveAccount.js'),
+		setActiveAccount : require('./util/setActiveAccount.js'),
+		serialize : require('./util/serialize.js')
+	}
+}
